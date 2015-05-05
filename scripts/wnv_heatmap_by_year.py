2d8706452473835a40b66300896dba23682f1f8f
shchur <oleks.shchur@gmail.com>
2015-05-05 13:51:05 +0200

    Split the WNV heatmap by year

--- scripts/wnv_heatmap_by_year.py
+++ scripts/wnv_heatmap_by_year.py
@@ -5,7 +5,7 @@
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 #%matplotlib inline
 
 from sklearn.neighbors import KernelDensity
@@ -13,14 +13,15 @@ from sklearn.neighbors import KernelDensity
 mapdata = np.loadtxt("../input_data/mapdata_copyright_openstreetmap_contributors.txt")
 traps = pd.read_csv('../input_data/train.csv', parse_dates=['Date'])[['Date', 'Trap','Longitude', 'Latitude', 'WnvPresent']]
 
-alpha_cm = pl.cm.Reds
+alpha_cm = plt.cm.Reds
 alpha_cm._init()
 alpha_cm._lut[:-3,-1] = abs(np.logspace(0, 1, alpha_cm.N) / 10 - 1)[::-1]
 aspect = mapdata.shape[0] * 1.0 / mapdata.shape[1]
 lon_lat_box = (-88, -87.5, 41.6, 42.1)
 
-pl.figure(figsize=(18,6))
+
 for year, subplot in zip([2007, 2009, 2011, 2013], [141, 142, 143, 144]):
+    plt.figure()
     sightings = traps[(traps['WnvPresent'] > 0) & (traps['Date'].apply(lambda x: x.year) == year)]
     sightings = sightings.groupby(['Date', 'Trap','Longitude', 'Latitude']).max()['WnvPresent'].reset_index()
     X = sightings[['Longitude', 'Latitude']].values
@@ -30,19 +31,15 @@ for year, subplot in zip([2007, 2009, 2011, 2013], [141, 142, 143, 144]):
     xv,yv = np.meshgrid(np.linspace(-88, -87.5, 100), np.linspace(41.6, 42.1, 100))
     gridpoints = np.array([xv.ravel(),yv.ravel()]).T
     zv = np.exp(kd.score_samples(gridpoints).reshape(100,100))
-    pl.subplot(subplot)
-    pl.gca().set_title(year)
-    pl.imshow(mapdata,
-               cmap=pl.get_cmap('gray'),
-               extent=lon_lat_box,
-               aspect=aspect)
-    pl.imshow(zv,
+    plt.gca().set_title(year)
+    plt.imshow(mapdata,
+               cmap=plt.get_cmap('gray'),
+               extent=lon_lat_box)
+    plt.imshow(zv,
                origin='lower',
                cmap=alpha_cm,
-               extent=lon_lat_box,
-               aspect=aspect)
-    pl.tight_layout()
+               extent=lon_lat_box)
     locations = traps[['Longitude', 'Latitude']].drop_duplicates().values
-    pl.scatter(locations[:,0], locations[:,1], marker='x')
+    plt.scatter(locations[:,0], locations[:,1], marker='x')
 
-pl.savefig('heatmap_by_year.png')
+    plt.savefig('../plots/heatmap_%d.png' % year, transparent=True)
